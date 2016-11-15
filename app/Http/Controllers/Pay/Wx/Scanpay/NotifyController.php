ec7160abe999ae6244e8a671b478f861c34c5ed2
wuoshiwzm <wuoshiwzm@hotmail.com>
2016-11-15 20:48:12 +0800

    11-15 wechat pay

--- app/Http/Controllers/Pay/Wx/Scanpay/NotifyController.php
+++ app/Http/Controllers/Pay/Wx/Scanpay/NotifyController.php
@@ -12,6 +12,10 @@ require_once app_path()."/Http/Wxpay/lib/WxPay.Api.php";
 require_once app_path().'/Http/Wxpay/lib/WxPay.Notify.php';
 require_once app_path().'/Http/Wxpay/example/log.php';
 
+
+
+
+
 class NotifyController extends Controller
 {
     public function index(){
@@ -20,6 +24,8 @@ class NotifyController extends Controller
       // $trade_state =$_GET ["trade_state"];//支付状态
       // $out_trade_no = $_GET ["out_trade_no"];//订单号
       //
+
+      PayNotifyCallBack::notifyReceive();
       $disk = Storage::disk('wxpay');
       $contents = $disk->append('wxpay.txt',' ');
 
@@ -30,38 +36,55 @@ class NotifyController extends Controller
     }
 }
 
-class PayNotifyCallBack extends WxPayNotify
+
+
+
+
+class PayNotifyCallBack extends \WxPayNotify
 {
-  //查询订单
-  public function Queryorder($transaction_id)
-  {
-    $input = new WxPayOrderQuery();
-    $input->SetTransaction_id($transaction_id);
-    $result = WxPayApi::orderQuery($input);
-    if(array_key_exists("return_code", $result)
-      && array_key_exists("result_code", $result)
-      && $result["return_code"] == "SUCCESS"
-      && $result["result_code"] == "SUCCESS")
-    {
-      return true;
-    }
-    return false;
-  }
 
-  //重写回调处理函数
-  public function NotifyProcess($data, &$msg)
-  {
-    $notfiyOutput = array();
 
-    if(!array_key_exists("transaction_id", $data)){
-      $msg = "输入参数不正确";
-      return false;
-    }
-    //查询订单，判断订单真实性
-    if(!$this->Queryorder($data["transaction_id"])){
-      $msg = "订单查询失败";
-      return false;
-    }
-    return true;
+  public static function notifyReceive(){
+    //初始化日志
+    $logHandler= new \CLogFileHandler(storage_path()."/app/wxpay/".date('Y-m-d').'.log');
+    $log = \Log::Init($logHandler, 15);
+
+    \Log::DEBUG("begin notify");
+    $notify = new PayNotifyCallBack();
+    $notify->Handle(false);
   }
+	//查询订单
+	public function Queryorder($transaction_id)
+	{
+		$input = new WxPayOrderQuery();
+		$input->SetTransaction_id($transaction_id);
+		$result = WxPayApi::orderQuery($input);
+		\Log::DEBUG("query:" . json_encode($result));
+		if(array_key_exists("return_code", $result)
+			&& array_key_exists("result_code", $result)
+			&& $result["return_code"] == "SUCCESS"
+			&& $result["result_code"] == "SUCCESS")
+		{
+			return true;
+		}
+		return false;
+	}
+
+	//重写回调处理函数
+	public function NotifyProcess($data, &$msg)
+	{
+		\Log::DEBUG("call back:" . json_encode($data));
+		$notfiyOutput = array();
+
+		if(!array_key_exists("transaction_id", $data)){
+			$msg = "输入参数不正确";
+			return false;
+		}
+		//查询订单，判断订单真实性
+		if(!$this->Queryorder($data["transaction_id"])){
+			$msg = "订单查询失败";
+			return false;
+		}
+		return true;
+	}
 }
