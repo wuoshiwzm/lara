9984333905947b50105f6e572f48c1e2c577660c
wuoshiwzm <wuoshiwzm@hotmail.com>
2016-11-16 11:22:35 +0800

    11-05

--- app/Http/Controllers/Pay/Wx/Scanpay/NotifyController.php
+++ app/Http/Controllers/Pay/Wx/Scanpay/NotifyController.php
@@ -19,13 +19,13 @@ require_once app_path().'/Http/Wxpay/example/log.php';
 class NotifyController extends Controller
 {
     public function index(){
-      // $postdata = file_get_contents("php://input");
+      $postdata = file_get_contents("php://input");
       // $postObj = simplexml_load_string ( $postdata, 'SimpleXMLElement', LIBXML_NOCDATA );
       // $trade_state =$_GET ["trade_state"];//支付状态
       // $out_trade_no = $_GET ["out_trade_no"];//订单号
-      PayNotifyCallBack::notifyReceive();
-      // $disk = Storage::disk('wxpay');
-      // $contents = $disk->append('wxpay.txt',' ');
+      // PayNotifyCallBack::notifyReceive();
+      $disk = Storage::disk('wxpay');
+      $contents = $disk->append('wxpay.txt',' ');
 
     }
 }
