7e67513d402dd91c20cb4c9a0f4f9348b2a60fe9
wuoshiwzm <wuoshiwzm@hotmail.com>
2016-11-17 12:58:14 +0800

    11-17

--- resources/views/admin/payment/js/wx_setpayment.blade.php
+++ resources/views/admin/payment/js/wx_setpayment.blade.php
@@ -9,7 +9,7 @@ function pay(a){
         itle: '付款请扫描以下二维码！',
         shadeClose: true,
         shade: 0.8,
-        area: ['500px', '50%'],
+        area: ['300px', '50%'],
         content: data //注意，如果str是object，那么需要字符拼接。
       });
    });
