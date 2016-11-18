cd1b9f8b41adbe17e9ef327b398e64795558b7ef
wuoshiwzm <wuoshiwzm@hotmail.com>
2016-11-18 17:53:00 +0800

    11-18

--- app/Http/Controllers/Social/WechatController.php
+++ app/Http/Controllers/Social/WechatController.php
@@ -34,7 +34,7 @@ class WechatController extends CommonController{
     //get the ACCESS TOKEN by each 7000ms
     $timeNow = time();
     $tokenData = Data::where('data_name','access_token')->orderBy('created_at','desc')->first();
-    dd($tokenData);
+    // dd($tokenData);
     // dd($timeNow - $tokenData->created_at->getTimeStamp());
     if (($timeNow - $tokenData->created_at->getTimeStamp())>3000)
     {
@@ -48,7 +48,7 @@ class WechatController extends CommonController{
     $data['data_name']='access_token';
     $output = json_decode($output);
     $data['data']=$output->access_token;
-    // dd($data);
+    dd($data);
     Data::create($data);
     return $output->access_token;
   }else{
