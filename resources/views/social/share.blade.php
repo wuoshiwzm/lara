8dfa94889f7225c4ee4b0883fd5dc665cba3efd3
wuoshiwzm <wuoshiwzm@hotmail.com>
2016-11-18 18:19:20 +0800

    11-18

--- resources/views/social/share.blade.php
+++ resources/views/social/share.blade.php
@@ -15,19 +15,17 @@
     <!-- already sign in -->
     <!-- <script type="text/javascript" src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script> -->
     <script>
-    alert("login already!");
-
       wx.config({
           debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
           appId: 'wx260619ea73a4b130',     // 必填，公众号的唯一标识
           timestamp: {{$wechat['timestamp']}}, // 必填，生成签名的时间戳
           nonceStr:  "{{$wechat['noncestr']}}", // 必填，生成签名的随机串
           signature: "{{$wechat['signature']}}",// 必填，签名，见附录1
-          jsApiList: ['checkJsApi','onMenuShareTimeline','onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+          jsApiList: ['onMenuShareTimeline'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
       });
 
       wx.ready(function(){
-
+   
         wx.onMenuShareTimeline({
             title: 'testtitle', // 分享标题
             link: 'adbangbang.com', // 分享链接
@@ -56,6 +54,7 @@
 
 
     </script>
+
     </div>
     <hr>
 
