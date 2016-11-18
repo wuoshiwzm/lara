d67f0c894d4c7e108e26606bd84c2f04f7d5f895
wuoshiwzm <wuoshiwzm@hotmail.com>
2016-11-18 18:23:28 +0800

    11-18

--- resources/views/social/share.blade.php
+++ resources/views/social/share.blade.php
@@ -14,30 +14,10 @@
           jsApiList: ['onMenuShareTimeline'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
       });
 
-      wx.ready(function(){
-
-        wx.onMenuShareTimeline({
-            title: 'testtitle', // 分享标题
-            link: 'adbangbang.com', // 分享链接
-            imgUrl: '', // 分享图标
-            success: function () {
-                alert(1111);// 用户确认分享后执行的回调函数
-            },
-            cancel: function () {
-              alert(222);  // 用户取消分享后执行的回调函数
-            }
-        });
-
-
-         // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相
-         //关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
-      });
-
-
 
       wx.error(function(res){
 
-        alert("failed!");
+        alert(res);
     // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
     });
 
@@ -54,7 +34,29 @@
     <!-- already sign in -->
     <!-- <script type="text/javascript" src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script> -->
 
+<script type="text/javascript">
+wx.ready(function(){
 
+  wx.onMenuShareTimeline({
+      title: 'testtitle', // 分享标题
+      link: 'adbangbang.com', // 分享链接
+      imgUrl: '', // 分享图标
+      success: function () {
+          alert(1111);// 用户确认分享后执行的回调函数
+      },
+      cancel: function () {
+        alert(222);  // 用户取消分享后执行的回调函数
+      }
+  });
+
+
+   // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相
+   //关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+});
+
+
+
+</script>
     </div>
     <hr>
 
