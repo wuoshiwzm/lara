2cfd43a9a1d558ca9d4d772219f68a07b86b966d
wuoshiwzm <wuoshiwzm@hotmail.com>
2016-11-19 14:19:33 +0800

    11-19

--- resources/views/social/share.blade.php
+++ resources/views/social/share.blade.php
@@ -22,19 +22,22 @@
     	});
 
     	wx.ready(function () {
+    		var shareData = {
+    			title: '这里是分享标题',
+    			desc: '无穷大分享 分享抢红包 分享有惊喜！',
+    			link: 'http://adbangbang.com/sharecontent/'+{{$media_id}},
+    			imgUrl: "{{asset('resources/views/home/images/logo.jpg')}}",
+          success: function () {
+              // 用户确认分享后执行的回调函数
+          },
+          cancel: function () {
+            alert('cancel');
+              // 用户取消分享后执行的回调函数
+          }
+    		};
 
-        wx.onMenuShareTimeline({
-            title: '这里是分享标题',
-            link: 'http://adbangbang.com/sharecontent/'+{{$media_id}},
-            imgUrl: "{{asset('resources/views/home/images/logo.jpg')}}"
-            success: function () {
-                // 用户确认分享后执行的回调函数
-            },
-            cancel: function () {
-                // 用户取消分享后执行的回调函数
-            }
-        });
-
+    		wx.onMenuShareAppMessage(shareData);
+    		wx.onMenuShareTimeline(shareData);
     	});
 
     	wx.error(function (res) {
