2e713afdd21eddc8bf0d5507c51decaaeca5a36f
wuoshiwzm <wuoshiwzm@hotmail.com>
2016-12-04 11:02:54 +0800

    12-4 workspace.xml

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,50 +1,47 @@
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>首页</title>
-<link rel="stylesheet" media="screen" href="{{asset('resources/views/home/css/style.css')}}" />
-<link rel="stylesheet" media="screen" href="{{asset('resources/views/home/css/jquery.jslides.css')}}" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>首页</title>
+    <link rel="stylesheet" media="screen" href="{{asset('resources/views/home/css/style.css')}}"/>
+    <link rel="stylesheet" media="screen" href="{{asset('resources/views/home/css/jquery.jslides.css')}}"/>
 
 
-<script type="text/javascript" src="{{asset('resources/views/home/js/jquery.js')}}"></script>
-<script type="text/javascript" src="{{asset('resources/views/home/js/jquery.jslides.js')}}"></script>
-<script type="text/javascript" src="{{asset('resources/views/home/js/link.js')}}"></script>
-<script type="text/javascript" src="{{asset('resources/views/home/js/script.js')}}"></script>
-<script type="text/javascript" src="{{asset('resources/org/layer/layer.js')}}"></script>
+    <script type="text/javascript" src="{{asset('resources/views/home/js/jquery.js')}}"></script>
+    <script type="text/javascript" src="{{asset('resources/views/home/js/jquery.jslides.js')}}"></script>
+    <script type="text/javascript" src="{{asset('resources/views/home/js/link.js')}}"></script>
+    <script type="text/javascript" src="{{asset('resources/views/home/js/script.js')}}"></script>
+    <script type="text/javascript" src="{{asset('resources/org/layer/layer.js')}}"></script>
 </head>
 
 <body>
-
-<?php
-  //dd(session('user'));
-?>
-  <body onload="time()">
-  <!-- data for weixin share -->
+ 
+<body onload="time()">
+<!-- data for weixin share -->
 
 
- <input type="hidden"  class="session_user"  value=
- @if( session('user'))
- "{{session('user')->user_name}}"
- @else
- ""
- @endif >
-  <div class="top">
-  <div class="wrappersy">
-      <div class="logo">
-      <a href=" {{url('')}}"><img src="{{asset('resources/views/home/images/logo.jpg')}}"></a>
-      </div>
-      <div class="top_right">
+<input type="hidden" class="session_user" value=
+@if( session('user'))
+        "{{session('user')->user_name}}"
+@else
+    ""
+@endif >
+<div class="top">
+    <div class="wrappersy">
+        <div class="logo">
+            <a href=" {{url('')}}"><img src="{{asset('resources/views/home/images/logo.jpg')}}"></a>
+        </div>
+        <div class="top_right">
 
 
-<!-- weather date ... -->
-@include('layouts.rig_top')
+            <!-- weather date ... -->
+        @include('layouts.rig_top')
 
-<!-- nav bar -->
-@include('layouts.nav')
+        <!-- nav bar -->
+        @include('layouts.nav')
 
-<!-- seach bar -->
-@include('layouts.search')
-</div>
-</div>
+        <!-- seach bar -->
+            @include('layouts.search')
+        </div>
+    </div>
 </div>
 <!-- 主横幅 -->
 @include('layouts.widget.mpic')
@@ -67,7 +64,7 @@
 
 <!-- 广告位 - 自媒体 -->
 <div class="guanggaowei">
-     <a href="{{url('self_media')}}"><img src="{{asset('resources/views/home/images/guanggao.jpg')}}" alt="" /></a>
+    <a href="{{url('self_media')}}"><img src="{{asset('resources/views/home/images/guanggao.jpg')}}" alt=""/></a>
 </div>
 
 <!-- 精品推荐 - 设计分类 -->
@@ -78,24 +75,24 @@
 <!-- 公司展示 -->
 <div class="gszs">
     <div class="img_pa">
-        <img src="images/aca.jpg" alt="" />
-        <img src="images/aca.jpg" alt="" />
-        <img src="images/aca.jpg" alt="" />
-        <img src="images/aca.jpg" alt="" />
-        <img src="images/aca.jpg" alt="" />
-        <img src="images/aca.jpg" alt="" />
-        <img src="images/aca.jpg" alt="" />
-        <img src="images/aca.jpg" alt="" />
-        <img src="images/aca.jpg" alt="" />
-        <img src="images/aca.jpg" alt="" />
-        <img src="images/aca.jpg" alt="" />
-        <img src="images/aca.jpg" alt="" />
-        <img src="images/aca.jpg" alt="" />
-        <img src="images/aca.jpg" alt="" />
+        <img src="images/aca.jpg" alt=""/>
+        <img src="images/aca.jpg" alt=""/>
+        <img src="images/aca.jpg" alt=""/>
+        <img src="images/aca.jpg" alt=""/>
+        <img src="images/aca.jpg" alt=""/>
+        <img src="images/aca.jpg" alt=""/>
+        <img src="images/aca.jpg" alt=""/>
+        <img src="images/aca.jpg" alt=""/>
+        <img src="images/aca.jpg" alt=""/>
+        <img src="images/aca.jpg" alt=""/>
+        <img src="images/aca.jpg" alt=""/>
+        <img src="images/aca.jpg" alt=""/>
+        <img src="images/aca.jpg" alt=""/>
+        <img src="images/aca.jpg" alt=""/>
     </div>
     <div class="clear"></div>
     <div class="more_box3">
-    	<a href=""><p>MORE</p></a>
+        <a href=""><p>MORE</p></a>
     </div>
 </div>
 
@@ -106,43 +103,42 @@
 
 {{--浏览记录--}}
 {{--<div class="bot">--}}
-    {{--<li>--}}
-        {{--<span class="fli">张三</span>--}}
-        {{--<span>13.20</span>--}}
-        {{--<span>浏览了</span>--}}
-        {{--<span><a href="">西安步旗文化广告传播公司</a></span>--}}
-        {{--<span>求购公司</span>--}}
-    {{--</li>--}}
-     {{--<li>--}}
-        {{--<span class="fli">张三</span>--}}
-        {{--<span>13.20</span>--}}
-        {{--<span>浏览了</span>--}}
-        {{--<span><a href="">西安步旗文化广告传播公司</a></span>--}}
-        {{--<span>求购公司</span>--}}
-    {{--</li>--}}
-     {{--<li>--}}
-        {{--<span class="fli">张三</span>--}}
-        {{--<span>13.20</span>--}}
-        {{--<span>浏览了</span>--}}
-        {{--<span><a href="">西安步旗文化广告传播公司</a></span>--}}
-        {{--<span>求购公司</span>--}}
-    {{--</li>--}}
+{{--<li>--}}
+{{--<span class="fli">张三</span>--}}
+{{--<span>13.20</span>--}}
+{{--<span>浏览了</span>--}}
+{{--<span><a href="">西安步旗文化广告传播公司</a></span>--}}
+{{--<span>求购公司</span>--}}
+{{--</li>--}}
+{{--<li>--}}
+{{--<span class="fli">张三</span>--}}
+{{--<span>13.20</span>--}}
+{{--<span>浏览了</span>--}}
+{{--<span><a href="">西安步旗文化广告传播公司</a></span>--}}
+{{--<span>求购公司</span>--}}
+{{--</li>--}}
+{{--<li>--}}
+{{--<span class="fli">张三</span>--}}
+{{--<span>13.20</span>--}}
+{{--<span>浏览了</span>--}}
+{{--<span><a href="">西安步旗文化广告传播公司</a></span>--}}
+{{--<span>求购公司</span>--}}
+{{--</li>--}}
 {{--</div>--}}
 
 {{--友情链接--}}
 {{--<div class="yl">--}}
-    {{--<div class="yl_con">--}}
-        {{--<a href="">西安步旗文化广告传播公司</a>--}}
-        {{--<a href="">西安步旗文化广告传播公司</a>--}}
-        {{--<a href="">西安步旗文化广告传播公司</a>--}}
-        {{--<a href="">西安步旗文化广告传播公司</a>--}}
-    {{--</div>--}}
+{{--<div class="yl_con">--}}
+{{--<a href="">西安步旗文化广告传播公司</a>--}}
+{{--<a href="">西安步旗文化广告传播公司</a>--}}
+{{--<a href="">西安步旗文化广告传播公司</a>--}}
+{{--<a href="">西安步旗文化广告传播公司</a>--}}
+{{--</div>--}}
 {{--</div>--}}
 
 
 
-        @include('layouts.footer')
-
+@include('layouts.footer')
 
 
 </body>
