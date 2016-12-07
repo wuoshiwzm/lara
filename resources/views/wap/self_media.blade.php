07a913f373080550a962281fb6912c5cc9887886
LAPTOP-SOIG1JSD\wuosh <wuoshiwzm@hotmail.com>
2016-12-07 16:25:15 +0800

    12-7

--- resources/views/wap/self_media.blade.php
+++ resources/views/wap/self_media.blade.php
@@ -39,6 +39,20 @@
             }
         });
         //        调取地址 --
+
+        //--调取地址
+        wx.getLocation({
+            success: function (res) {
+                var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+                var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+                var speed = res.speed; // 速度，以米/每秒计
+                var accuracy = res.accuracy; // 位置精度
+            },
+            cancel: function (res) {
+                alert('用户拒绝授权获取地理位置');
+            }
+        });
+        //--调取地址
     });
 
 </script>
