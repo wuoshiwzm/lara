cd15f44fda5426294a7c5664d7f1d1649f2cfc13
LAPTOP-SOIG1JSD\wuosh <wuoshiwzm@hotmail.com>
2016-12-07 16:41:58 +0800

    12-7

--- resources/views/wap/self_media.blade.php
+++ resources/views/wap/self_media.blade.php
@@ -13,11 +13,11 @@
 <body>
 <script>
     wx.config({
-        debug: true,
+        debug: false,
         appId:"<?php echo $appid;?>",
         timestamp:"<?php echo $timestamp;?>",
         nonceStr:"<?php echo $nonceStr;?>" ,
-        signature:'<?php echo $signature;?>',
+        signature:"<?php echo $signature;?>",
         jsApiList: [
             // 所有要调用的 API 都要加到这个列表中
             'checkJsApi',
