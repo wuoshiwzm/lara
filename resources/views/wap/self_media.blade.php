e0e5dc0e3b3eedb4c3ba0c47847d9094318113e9
LAPTOP-SOIG1JSD\wuosh <wuoshiwzm@hotmail.com>
2016-12-07 17:21:55 +0800

    12-7

--- resources/views/wap/self_media.blade.php
+++ resources/views/wap/self_media.blade.php
@@ -51,7 +51,7 @@
                 var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
                 var speed = res.speed; // 速度，以米/每秒计
                 var accuracy = res.accuracy; // 位置精度
-                alert('经纬度：'+longitude+'===>'+latitude);
+                alert(accuracy);
             },
             cancel: function (res) {
                 alert('用户拒绝授权获取地理位置');
