fb82977879ec438e2a12b77133e606a578cadebd
LAPTOP-SOIG1JSD\wuosh <wuoshiwzm@hotmail.com>
2016-12-07 17:09:01 +0800

    12-7

--- resources/views/wap/self_media.blade.php
+++ resources/views/wap/self_media.blade.php
@@ -13,7 +13,7 @@
 <body>
 <script>
     wx.config({
-        debug: false,
+        debug: true,
         appId:"{{$appid}}",
         timestamp:"{{$timestamp}}",
         nonceStr:"{{$nonceStr}}" ,
@@ -25,20 +25,6 @@
             'getLocation'
         ]
     });
-    //--调取地址
-    wx.getLocation({
-        success: function (res) {
-            var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-            var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-            var speed = res.speed; // 速度，以米/每秒计
-            var accuracy = res.accuracy; // 位置精度
-
-        },
-        cancel: function (res) {
-            alert('用户拒绝授权获取地理位置');
-        }
-    });
-    //--调取地址
 
     wx.ready(function () {
 
@@ -57,8 +43,6 @@
             }
         });
         //调取地址
-
-
 
         //--调取地址
         wx.getLocation({
