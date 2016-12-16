e218d43f3fb7be7bca48f489b417be53304dd453
wuoshiwzm <wuoshiwzm@hotmail.com>
2016-12-16 22:11:30 +0800

    12-16

--- resources/views/wap/self_media.blade.php
+++ resources/views/wap/self_media.blade.php
@@ -44,7 +44,7 @@
     <script>
 
         wx.config({
-            debug: false,
+            debug: true,
             appId: "{{$appid}}",
             timestamp: "{{$timestamp}}",
             nonceStr: "{{$nonceStr}}",
@@ -89,13 +89,13 @@
                                 'latitude': latitude,
                                 'longitude': longitude,
                                 'accuracy': accuracy,
-                                '_token': "{{csrf_token()}}",
+                                '_token': {{csrf_token()}},
 
 
                             }, function (data) {
                                 //得到内容的JSON 字符串，解析并显示
+alert(data);
 
-                                alert(1);
                                 //解析 json字符串
                                 var data = $.parseJSON(data);
                                 $.each(data, function (n, value) {
