bff0434729df3582185b93dcda0b319be086a7fb
wuoshiwzm <wuoshiwzm@hotmail.com>
2016-12-17 18:53:03 +0800

    12-17

--- app/Http/Routes/WapRoutes.php
+++ app/Http/Routes/WapRoutes.php
@@ -10,7 +10,7 @@
 Route::get('/','IndexController@index');
 
 //自媒体
-Route::get('/self_media1/{code?}/{scope?}/{state?}/{other?}','IndexController@selfMedia');
+Route::get('/self_media1/{code?}/{state?}','IndexController@selfMedia');
 
 //自媒体 地址经纬度获取并更新对应数据
 Route::any('/self_media/get_content','IndexController@getContent');
