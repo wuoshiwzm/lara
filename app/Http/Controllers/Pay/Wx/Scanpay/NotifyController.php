7203e97fc89460af2b74a7f328063ffd0a7df34b
LAPTOP-SOIG1JSD\wuosh <wuoshiwzm@hotmail.com>
2016-12-19 21:49:32 +0800

    12-19

--- app/Http/Controllers/Pay/Wx/Scanpay/NotifyController.php
+++ app/Http/Controllers/Pay/Wx/Scanpay/NotifyController.php
@@ -81,15 +81,13 @@ class NotifyController extends Controller
 
         //if $num == 0 , means there is no such order. them write to databaese
         if (!$num && $msg['mch_id'] == $this->mchid) {
-
 //            Payment::create($payment);
             //事务处理
             DB::transaction(function () use ($payment,$userBalance) {
                 Payment::create($payment);
-                $userName = $payment['payment_user_name'];
-                User::where('user_name', $userName)
-                    ->increment('user_balance', $userBalance);;
-
+//                $userName = $payment['payment_user_name'];
+//                User::where('user_name', $userName)
+//                    ->increment('user_balance', $userBalance);
             });
         }
     }
