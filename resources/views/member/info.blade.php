2231b72773509a938818b118b8a347a1cff436cb
LAPTOP-SOIG1JSD\wuosh <wuoshiwzm@hotmail.com>
2016-12-20 14:18:58 +0800

    12-19

--- resources/views/member/info.blade.php
+++ resources/views/member/info.blade.php
@@ -1,44 +1,53 @@
 @extends('layouts.admin')
 
 @section('content')
+    <table class="add_tab">
+        <tbody>
+
+        <tr>
+            <th width="30%"><i class="fa fa-fw fa-clipboard"></i>目前账户额度</th>
+            <td> {{Session::get('user')->user_balance}}</td>
+        </tr>
+
+        <tr>
+            <th width="30%"><i class="fa fa-fw fa-clipboard"></i></th>
+            <td><a href="{{url('admin/company')}}" target="main">完善企业信息</a></td>
+        </tr>
 
 
+        <tr>
+            <th width="30%"><i class="fa fa-fw fa-briefcase"></i></th>
+            <td><a href="{{url('admin/pass')}}" target="main">公司资源管理</a></td>
+        </tr>
+
+        <tr>
+            <th width="30%"><i class="fa fa-fw fa-bullhorn"></i></th>
+            <td><a href="{{url('admin/pass')}}" target="main">公司新闻发布</a></td>
+        </tr>
+
+        <tr>
+            <th width="30%"><i class="fa fa-fw fa-child"></i></th>
+            <td><a href="{{url('admin/pass')}}" target="main">公司招聘</a></td>
+        </tr>
+
+        <tr>
+            <th width="30%"><i class="fa fa-fw fa-certificate"></i></th>
+            <td><a href="{{url('admin/pass')}}" target="main">公司认证</a></td>
+        </tr>
+
+        <tr>
+            <th width="30%"><i class="fa fa-fw fa-thumbs-o-up"></i></th>
+            <td><a href="{{url('admin/pass')}}" target="main">公司合作客户</a></td>
+        </tr>
+
+        <tr>
+            <th width="30%"><i class="fa fa-fw fa-fax"></i></th>
+            <td><a href="{{url('admin/pass')}}" target="main">公司详细联系方式</a></td>
+        </tr>
 
 
-<ul>
+        </tbody>
+    </table>
 
-	<li>
-		<h3><i class="fa fa-fw fa-clipboard"></i><a href="{{url('admin/company')}}"  target="main">完善企业信息</a></h3>
-
-	</li>
-
-	<li>
-		<h3><i class="fa fa-fw fa-briefcase"></i><a href="{{url('admin/pass')}}"  target="main">公司资源管理</a></h3>
-
-	</li>
-
-	<li>
-		<h3><i class="fa fa-fw fa-bullhorn"></i><a href="{{url('admin/pass')}}"  target="main">公司新闻发布</a></h3>
-
-	</li>
-
-	<li>
-		<h3><i class="fa fa-fw fa-child"></i><a href="{{url('admin/pass')}}"  target="main">公司招聘</a></h3>
-
-	</li>
-
-	<li>
-		<h3><i class="fa fa-fw fa-certificate"></i><a href="{{url('admin/pass')}}"  target="main">公司认证</a></h3>
-	</li>
-
-	<li>
-		<h3><i class="fa fa-fw fa-thumbs-o-up"></i><a href="{{url('admin/pass')}}"  target="main">公司合作客户</a></h3>
-	</li>
-
-	<li>
-		<h3><i class="fa fa-fw fa-fax"></i><a href="{{url('admin/pass')}}"  target="main">公司详细联系方式</a></h3>
-	</li>
-</ul>
-
-	<!--结果集列表组件 结束-->
+    <!--结果集列表组件 结束-->
 @endsection
