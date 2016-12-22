f319c3b16d95610ffe9ccde2df7f47604f369e95
LAPTOP-SOIG1JSD\wuosh <wuoshiwzm@hotmail.com>
2016-12-22 17:17:34 +0800

    12-22

--- resources/views/layouts/widget/investment.blade.php
+++ resources/views/layouts/widget/investment.blade.php
@@ -10,34 +10,36 @@
         <div id="con_lef_3" class="hover" style="display: block;">
             <div class="con_box4">
                 @if(isset($news))
-                @foreach($news as $k => $v)
-                    @if($k == 0)
+                    @foreach($news as $k => $v)
+                        @if($k == 0)
 
-                        <div class="con_left">
-                            <a href=""><img src="images/image1.jpg"/></a>
-                            <a href=""><h1>{{$v->news_title}}...</h1></a>
-                            <p><?php echo substr($v->news_content, 0, 15) ?>...<span><a href=""
-                                                                                        class="red">[详情]</a></span></p>
-                        </div>
-                    @endif
-                @endforeach
+                            <div class="con_left">
+                                <a href=""><img src="images/image1.jpg"/></a>
+                                <a href=""><h1>{{$v->news_title}}...</h1></a>
+                                <p><?php echo substr($v->news_content, 0, 15) ?>...<span><a href=""
+                                                                                            class="red">[详情]</a></span>
+                                </p>
+                            </div>
+                        @endif
+                    @endforeach
                 @endif
 
 
                 <div class="con_right">
                     @if(isset($news))
-                    @foreach($news as $k => $v)
-                        <div class="li_con">
-                            <span class="span_num">0{{$k}}</span>
-                            <div class="squ"></div>
-                            <div class="li_conrg">
-                                <p class="li_title"><a href="">{{$v->news_title}}...</a><span>2016-05-04</span></p>
-                                <p class="li_jianjie"><a href=""><?php echo substr($v->news_content, 0, 15) ?>...</a>
-                                </p>
+                        @foreach($news as $k => $v)
+                            <div class="li_con">
+                                <span class="span_num">0{{$k}}</span>
+                                <div class="squ"></div>
+                                <div class="li_conrg">
+                                    <p class="li_title"><a href="">{{$v->news_title}}...</a><span>2016-05-04</span></p>
+                                    <p class="li_jianjie"><a href=""><?php echo substr($v->news_content, 0, 15) ?>
+                                            ...</a>
+                                    </p>
+                                </div>
                             </div>
-                        </div>
-                    @endforeach
-                        @endif
+                        @endforeach
+                    @endif
                 </div>
             </div>
 
@@ -70,10 +72,12 @@
 
 
             <ul class="lii">
-                @foreach($offer as $k => $v)
-                    <li><a href="">{{$v->offer_title}}
-                            ...<span><?php echo date('Y-m-d', strtotime($v->created_at))?></span></a></li>
-                @endforeach
+                @if(isset($offer))
+                    @foreach($offer as $k => $v)
+                        <li><a href="">{{$v->offer_title}}
+                                ...<span><?php echo date('Y-m-d', strtotime($v->created_at))?></span></a></li>
+                    @endforeach
+                @endif
             </ul>
         </div>
 
