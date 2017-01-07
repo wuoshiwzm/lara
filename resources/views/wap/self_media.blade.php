e1417320c131056eaa492000857a372a3edf8c33
miaoqi-leo <miaoqi-leo@hotmail.com>
2017-01-07 11:43:09 +0800

    调试全民推广

--- resources/views/wap/self_media.blade.php
+++ resources/views/wap/self_media.blade.php
@@ -114,6 +114,7 @@
                         var wxlocationlng = res.longitude; // 经度，浮点数，范围为180 ~ -180。
                         // var speed = res.speed; // 速度，以米/每秒计
                         // var accuracy = res.accuracy; // 位置精度
+                        alert(latitude+','+longitude);
                         getShareList(wxlocationlat, wxlocationlng);
                     },
                     cancel: function (res) {
